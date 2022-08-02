37f4bfec70f3c00aebe8e16706ef88009acd5e30
kobayashi shun <g2053417@cc.kyoto-su.ac.jp>
2022-08-02 09:40:28 +0900

    オブジェクト指向にリファクタリング

--- MorseCode2/MorseCode2.py
+++ MorseCode2/MorseCode2.py
@@ -6,8 +6,8 @@
 """
 
 __author__ = 'Kobayashi Shun'
-__version__ = '1.0.0'
-__date__ = '2022/7/29 (Created: 2022/07/25)'
+__version__ = '1.0.2'
+__date__ = '2022/08/02 (Created: 2022/07/25)'
 
 import MeCab as mecab
 
@@ -17,50 +17,54 @@ class MorseCode2:
     入力された文をモールス信号に変換する。
     """
 
-    string = None
-
-    reading = ''
-
-    morse_dictionary = {'ア': '--.--', 'イ': '.-', 'ウ': '..-', 'エ': '-.---', 'オ': '.-...',
-                        'カ': '.-..', 'キ': '-.-..', 'ク': '...-', 'ケ': '-.--', 'コ': '----',
-                        'サ': '-.-.-', 'シ': '--.-.', 'ス': '---.-', 'セ': '.---.', 'ソ': '---.',
-                        'タ': '-.', 'チ': '..-.', 'ツ': '.--.', 'テ': '.-.--', 'ト': '..-..',
-                        'ナ': '.-.', 'ニ': '-.-.', 'ヌ': '....', 'ネ': '--.-', 'ノ': '..--',
-                        'ハ': '-...', 'ヒ': '--..-', 'フ': '--..', 'ヘ': '.', 'ホ': '-..',
-                        'マ': '-..-', 'ミ': '..-.-', 'ム': '-', 'メ': '-...-', 'モ': '-..-.',
-                        'ヤ': '.--', 'ユ': '-..--', 'ヨ': '--',
-                        'ラ': '...', 'リ': '--.', 'ル': '-.--.', 'レ': '---', 'ロ': '.-.-',
-                        'ワ': '-.-', 'ヲ': '.---', 'ン': '.-.-.',
-                        'ー': '.--.-', '、': '.-.-.-', '゛': '..', '゜': '..--.'
-                        }
-
-    dakuonpu_dictionary = {'ガ': 'カ゛', 'ギ': 'キ゛', 'グ': 'ク゛', 'ゲ': 'ケ゛', 'ゴ': 'コ゛',
-                           'ザ': 'サ゛', 'ジ': 'シ゛', 'ズ': 'ス゛', 'ゼ': 'セ゛', 'ゾ': 'ソ゛',
-                           'ダ': 'タ゛', 'ヂ': 'チ゛', 'ヅ': 'ツ゛', 'デ': 'テ゛', 'ド': 'ト゛',
-                           'バ': 'ハ゛', 'ビ': 'ヒ゛', 'ブ': 'フ゛', 'ベ': 'ヘ゛', 'ボ': 'ホ゛',
-                           'パ': 'ハ゜', 'ピ': 'ヒ゜', 'プ': 'フ゜', 'ペ': 'ヘ゜', 'ポ': 'ホ゜',
-                           }
-
     def __init__(self, text):
         """
         インスタンスを生成します。
         """
-        self.string = text
+        self._string = text
+        self._reading = ''
+        self._morse = ''
+        self._morse_dictionary = {'ア': '--.--', 'イ': '.-', 'ウ': '..-', 'エ': '-.---', 'オ': '.-...',
+                                  'カ': '.-..', 'キ': '-.-..', 'ク': '...-', 'ケ': '-.--', 'コ': '----',
+                                  'サ': '-.-.-', 'シ': '--.-.', 'ス': '---.-', 'セ': '.---.', 'ソ': '---.',
+                                  'タ': '-.', 'チ': '..-.', 'ツ': '.--.', 'テ': '.-.--', 'ト': '..-..',
+                                  'ナ': '.-.', 'ニ': '-.-.', 'ヌ': '....', 'ネ': '--.-', 'ノ': '..--',
+                                  'ハ': '-...', 'ヒ': '--..-', 'フ': '--..', 'ヘ': '.', 'ホ': '-..',
+                                  'マ': '-..-', 'ミ': '..-.-', 'ム': '-', 'メ': '-...-', 'モ': '-..-.',
+                                  'ヤ': '.--', 'ユ': '-..--', 'ヨ': '--',
+                                  'ラ': '...', 'リ': '--.', 'ル': '-.--.', 'レ': '---', 'ロ': '.-.-',
+                                  'ワ': '-.-', 'ヲ': '.---', 'ン': '.-.-.',
+                                  'ー': '.--.-', '、': '.-.-.-', '゛': '..', '゜': '..--.'
+                                  }
 
-    def get_pronunciation(self, text):
-        """文を受け取り、その読み方をカタカナに変換して出力する。
+    def get_reading(self) -> str:
+        """
+        カタカナの読みを返す
 
-        Args:
-                text (String): 文
+        Returns:
+            _reading: カタカナ文
+        """
+        return self._reading
+
+    def get_morse(self) -> str:
+        """
+        モールス信号を返す
+
+        Returns:
+            _morse: モールス信号
+        """
+        return self._morse
+
+    def pronunciation(self):
+        """
+        文を受け取り、その読み方をカタカナに変換して出力する。
 
         Returns:
                 String: 受け取った文字列の読み方をカタカナで返す
         """
         m = mecab.Tagger()
 
-        print(self.string)
-
-        result = m.parse(self.string).splitlines()  # mecabの解析結果の取得
+        result = m.parse(self._string).splitlines()  # mecabの解析結果の取得
         result = result[:-1]  # EOFの排除
 
         for vocabulary in result:
@@ -68,48 +72,49 @@ class MorseCode2:
                 continue
             # surface = vocabulary.split('\t')[0]  # 表層系を取得
             ruby = vocabulary.split('\t')[1].split(',')[-2]  # ルビ(読み方)を取得
-            self.reading += ruby
+            self._reading += ruby
 
-        print('reading: ' + self.reading)
+        self.dakuon_transfer()
+        self.shouon_transfer()
 
-        self.reading = self.dakuon_transfer(self.reading)
-
-        return self.reading
-
-    def dakuon_transfer(self, a_string):
-        """濁点・半濁点を変換する
-        Args:
-                list (_type_): _description_
-        Returns:
-                _type_: _description_
+    def dakuon_transfer(self):
         """
-        print('aString: ' + a_string)
-        list_of_dakuonpu = self.dakuonpu_dictionary.keys()
+        濁点・半濁点を変換する
+        """
+        dakuonpu_dictionary = {'ガ': 'カ゛', 'ギ': 'キ゛', 'グ': 'ク゛', 'ゲ': 'ケ゛', 'ゴ': 'コ゛',
+                               'ザ': 'サ゛', 'ジ': 'シ゛', 'ズ': 'ス゛', 'ゼ': 'セ゛', 'ゾ': 'ソ゛',
+                               'ダ': 'タ゛', 'ヂ': 'チ゛', 'ヅ': 'ツ゛', 'デ': 'テ゛', 'ド': 'ト゛',
+                               'バ': 'ハ゛', 'ビ': 'ヒ゛', 'ブ': 'フ゛', 'ベ': 'ヘ゛', 'ボ': 'ホ゛',
+                               'パ': 'ハ゜', 'ピ': 'ヒ゜', 'プ': 'フ゜', 'ペ': 'ヘ゜', 'ポ': 'ホ゜',
+                               }
+        list_of_dakuonpu = dakuonpu_dictionary.keys()
         list_of_dakuonpu = list(list_of_dakuonpu)
         for char in list_of_dakuonpu:
-            if char in a_string:
-                a_string = a_string.replace(
-                    char, self.dakuonpu_dictionary.get(char))
+            if char in self._reading:
+                self._reading = self._reading.replace(
+                    char, dakuonpu_dictionary.get(char))
 
-        return a_string
-
-    def string_to_morse(self, text):
-        """カタカナ文を受け取り、モールス信号変換して出力する。
-
-        Args:
-                text (String): カタカナ文
-
-        Returns:
-                String: モールス信号
+    def shouon_transfer(self):
         """
-        answer = ""
-        for char in text:
+        小音を変換する
+        """
+        shouon_dictionary = {'ッ': 'ツ', 'ャ': 'ヤ', 'ュ': 'ユ', 'ョ': 'ヨ'}
+        list_of_shouon = shouon_dictionary.keys()
+        list_of_shouon = list(list_of_shouon)
+        for char in list_of_shouon:
+            if char in self._reading:
+                self._reading = self._reading.replace(
+                    char, shouon_dictionary.get(char))
+
+    def string_to_morse(self):
+        """
+        カタカナ文を受け取り、モールス信号変換して出力する。
+        """
+        for char in self._reading:
             try:
-                answer += self.morse_dictionary[char] + "   "
+                self._morse += self._morse_dictionary[char] + "   "
             except KeyError:
                 print('\" ' + char + ' " is not in Morse Dictionary.')
-
-        return answer
 
 
 def main():
@@ -117,13 +122,14 @@ def main():
     入力された文をモールス信号に変換するプログラムです。
     常に0を応答します。それが結果（リターンコード：終了ステータス）になることを想定しています。
     """
-    input_str = input("モールス信号に変換する文を入力してください: ")
+    print("モールス信号に変換する文を入力してください")
+    input_str = input(">")
     morse = MorseCode2(input_str)
 
-    reading = morse.get_pronunciation(input_str)
-    print(reading)
-    answer = morse.string_to_morse(reading)
-    print(answer)
+    morse.pronunciation()
+    print(morse.get_reading())
+    morse.string_to_morse()
+    print(morse.get_morse())
 
     return 0
 
